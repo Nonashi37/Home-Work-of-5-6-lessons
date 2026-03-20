7df6a66a8b9c64645ed115c4bf6831b33fce5a4d
Nonashi37 <argeno37@gmail.com>
2026-03-20 13:34:00 +0600

    Initial commit

---
+++ db/main_db.py
@@ -0,0 +1,60 @@
+import sqlite3
+from db import queries
+from config import path_db
+
+
+def init_db():
+    conn = sqlite3.connect(path_db)
+    cursor = conn.cursor()
+    cursor.execute(queries.task_table)
+    print('БД подключена!')
+    conn.commit()
+    conn.close()
+
+
+
+
+def add_task(task):
+    conn = sqlite3.connect(path_db)
+    cursor = conn.cursor()
+    cursor.execute(queries.insert_task, (task, ))
+    conn.commit()
+    task_id = cursor.lastrowid 
+    conn.close()
+    return task_id
+
+
+# def add_task(task):
+#     with sqlite3.connect(path_db) as conn:
+#         cursor = conn.cursor()
+#         cursor.execute(queries.insert_task, (task, ))
+#         task_id = cursor.lastrowid
+
+
+def update_task(task_id, new_task=None, completed=None):
+    conn = sqlite3.connect(path_db)
+    cursor = conn.cursor()
+
+    if new_task is not None:
+        cursor.execute(queries.update_task, (new_task, task_id))
+    elif completed is not None:
+        cursor.execute("UPDATE tasks SET completed = ? WHERE id = ?", (completed, task_id))
+
+    conn.commit()
+    conn.close()
+
+# all - completed - uncompleted
+def get_tasks(filter_type):
+    conn = sqlite3.connect(path_db)
+    cursor = conn.cursor()
+
+    if filter_type == 'all':
+        cursor.execute(queries.select_task)
+    elif filter_type == 'completed':
+        cursor.execute(queries.select_task_completed)
+    elif filter_type == 'uncompleted':
+        cursor.execute(queries.select_task_uncompleted)
+
+    tasks = cursor.fetchall()
+    conn.close()
+    return tasks 
---
+++ db/queries.py
@@ -0,0 +1,24 @@
+task_table = """
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        task TEXT NOT NULL,
+        completed INTEGER DEFAULT 0 
+    )
+"""
+
+# CRUD - CREATE - READ - UPDATE - DELETE
+# INSERT SELECT UPDATE DELETE
+
+insert_task = "INSERT INTO tasks (task) VALUES (?)"
+
+select_task = 'SELECT id, task, completed FROM tasks'
+
+select_task_completed = 'SELECT id, task, completed FROM tasks WHERE completed = 1'
+select_task_uncompleted = 'SELECT id, task, completed FROM tasks WHERE completed = 0'
+
+update_task = "UPDATE tasks SET task = ? WHERE id = ?"
+
+delete_task = "DELETE FROM tasks WHERE id = ?"
+
+
+
